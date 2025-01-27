e8a77bb054c528e6d1c589718503e5b3447ab5bd
DiegoZC <dizuco25@gmail.com>
2025-01-27 19:51:37 +0100

    Intento de implementar las animaciones
    Intento de que Ziggs tenga las animaciones

---
+++ Assets/Scripts/PlayerAnimation.cs
@@ -0,0 +1,22 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerAnimation : MonoBehaviour
+{
+    private Animator animator;
+    private PlayerMovement playerMovement;
+    private float directionDampTime = 0.25f;
+
+    private void Start()
+    {
+        animator = GetComponent<Animator>();
+        playerMovement = GetComponent<PlayerMovement>();
+    }
+
+    private void LateUpdate()
+    {
+        animator.SetFloat("Speed", playerMovement.GetCurrentSpeed() / playerMovement.runningSpeed);
+        animator.SetFloat("Direction", directionDampTime);
+    }
+}
